f97595169f28f87d8f13993cfaebe79d01701d3a
Paola2005 <137318847+Paola2005@users.noreply.github.com>
2023-10-12 12:23:00 +0000

    .

--- Infrastructura/Data/NotiContext.cs
+++ Infrastructura/Data/NotiContext.cs
@@ -2,11 +2,17 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Core.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructura.Data
 {
-    public class NotiContext
+    public class NotiContext : DbContext
     {
+        public NotiContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Auditoria>Auditorias{get; set;}
         
     }
 }
